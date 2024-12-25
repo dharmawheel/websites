68d4d29072c0c38a008964a129b6075cfe41f46f
Zelly Snyder <zelcon@zelcon.net>
2024-12-24 23:39:11 -0800

    Set up deployment with Github Actions

---
+++ terraform/github_actions_role.tf
@@ -0,0 +1,91 @@
+resource "aws_iam_role" "github_actions_role" {
+  name               = "github-actions-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "sts.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "github_actions_policy" {
+  name        = "github-actions-policy"
+  description = "Policy for GitHub Actions CI/CD tasks"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssm:SendCommand",
+        "ssm:ListCommandInvocations",
+        "ssm:GetCommandInvocation",
+        "ssm:DescribeInstanceInformation",
+        "ssm:StartSession"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeInstances",
+        "ec2:ModifyLaunchTemplate"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:StartInstanceRefresh",
+        "autoscaling:DescribeAutoScalingGroups"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "arn:aws:s3:::${aws_s3_bucket.tfstate_bucket.bucket}",
+        "arn:aws:s3:::${aws_s3_bucket.tfstate_bucket.bucket}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "dynamodb:*"
+      ],
+      "Resource": "${aws_dynamodb_table.tfstate_lock_table.arn}"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_github_actions_policy" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = aws_iam_policy.github_actions_policy.arn
+}
+
+resource "aws_iam_user_policy_attachment" "github_actions_user_policy_attachment" {
+  user       = aws_iam_user.github_actions_user.name
+  policy_arn = aws_iam_policy.github_actions_policy.arn
+}
+
+resource "aws_iam_access_key" "github_actions_access_key" {
+  user = aws_iam_user.github_actions_user.name
+}
+
+resource "aws_iam_user" "github_actions_user" {
+  name = "github-actions-user"
+}
